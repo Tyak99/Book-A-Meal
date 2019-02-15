20e11b4968d59d5f3f725c44a50324c6ebcdf081
Tunde Yakub <tundenasri@gmail.com>
2019-02-15 08:21:26 +0100

    added prettier file to project

--- server/services/MealService.js
+++ server/services/MealService.js
@@ -1,32 +1,32 @@
-import Meals from "../models/Meals";
+import Meals from '../models/Meals';
 
 export default class MealService {
   fetchAllMeals() {
     this.meals = [
       {
         id: 1,
-        name: "Jollof Rice",
-        size: "plates",
-        price: "500",
-        currency: "NGN"
+        name: 'Jollof Rice',
+        size: 'plates',
+        price: '500',
+        currency: 'NGN',
       },
       {
         id: 2,
-        name: "Fried Rice",
-        size: "plates",
-        price: "500",
-        currency: "NGN"
+        name: 'Fried Rice',
+        size: 'plates',
+        price: '500',
+        currency: 'NGN',
       },
       {
         id: 3,
-        name: "Coconut Rice",
-        size: "plates",
-        price: "500",
-        currency: "NGN"
-      }
+        name: 'Coconut Rice',
+        size: 'plates',
+        price: '500',
+        currency: 'NGN',
+      },
     ];
 
-    return this.meals.map(data => {
+    return this.meals.map((data) => {
       const meal = new Meals();
       meal.id = data.id;
       meal.name = data.name;
@@ -35,9 +35,11 @@ export default class MealService {
       return meal;
     });
   }
+
   getAll() {
     return this.fetchAllMeals();
   }
+
   get(id) {
     return this.fetchAllMeals()[id - 1];
   }
