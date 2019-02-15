c0fa2e010c056dabf0910cc5a98cd30dcd251c0c
Tunde Yakub <tundenasri@gmail.com>
2019-02-15 15:10:24 +0100

    added post route to order routes

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -8,4 +8,13 @@ router.get('/', (req, res) => {
   res.status(200).send(orderServices.getAll());
 });
 
+router.post('/', (req, res) => {
+  const newOrder = {
+    meals: req.body.meals,
+    price: req.body.price,
+    customer: req.user.id,
+  };
+  res.status(202).send(orderServices.addOrder(newOrder));
+});
+
 export default router;
