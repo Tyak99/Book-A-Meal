a4b9acdc7ce2346f8528f0310423692ef62e9204
Tunde Yakub <tundenasri@gmail.com>
2019-02-24 21:36:06 +0100

    created a home route and template for listing the routes

--- server/app.js
+++ server/app.js
@@ -8,10 +8,15 @@ import orderRoutes from './routes/orderRoutes';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
 
 app.use('/api/v1/meals', mealRoutes);
 app.use('/api/v1/menu', menuRoutes);
 app.use('/api/v1/orders', orderRoutes);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
