9dba8c4efa696885977ec87c6530f9d150ede381
Tunde Yakub <tundenasri@gmail.com>
2019-02-24 21:48:27 +0100

    added dotenv to project for easy usage of .env file

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
 // routes
 import mealRoutes from './routes/mealsRoute';
 import menuRoutes from './routes/menusRoute';
@@ -18,9 +18,7 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log('port has started');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
 module.exports = app;
