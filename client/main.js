df9b3118225ba03f3fb68ad2a0492dacf4cf37cf
Tunde Yakub <tundenasri@gmail.com>
2019-02-24 11:46:03 +0100

    created the page for caterer to add meals

--- client/main.js
+++ client/main.js
@@ -1,3 +1,8 @@
+let modal = document.querySelector('#myModal');
+let openModalButton = document.querySelector('#openModal');
+let closeModalButton = document.querySelector('.close-button');
+let submitMeal = document.querySelector('.submit-meal');
+
 function myFunction() {
     var navbar = document.getElementById("myNav");
     if (navbar.className === "nav") {
@@ -6,3 +11,18 @@ function myFunction() {
       navbar.className = "nav";
     }
   }
+
+  function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+      toggleModal();
+  }
+}
+
+openModalButton.addEventListener('click', toggleModal);
+closeModalButton.addEventListener('click', toggleModal);
+submitMeal.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
