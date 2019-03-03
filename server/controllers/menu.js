e0b4e830c7b108950a6f5d8600a937b57ab62530
Tunde Yakub <tundenasri@gmail.com>
2019-03-03 10:06:46 +0100

    create the adding menu to the database

--- server/controllers/menu.js
+++ server/controllers/menu.js
@@ -2,6 +2,26 @@ import Model from '../models';
 
 const { Menu } = Model;
 
-exports.test = ('/', (req, res) => {
+exports.test = ('/',
+(req, res) => {
   res.send('controllll');
+});
+
+exports.create = ('/',
+(req, res) => {
+  Menu.create({
+    name: req.body.name,
+    price: req.body.price,
+    meals: req.body.meals,
+    catererId: req.body.caterer,
+  })
+    .then((menu) =>
+      res.send({
+        status: 201,
+        data: menu,
+      })
+    )
+    .catch((error) =>
+      res.send({ status: 400, error: 'Could not create menu' })
+    );
 });
--- server/routes/menu.js
+++ server/routes/menu.js
@@ -4,5 +4,6 @@ import menuController from '../controllers/menu';
 const router = express();
 
 router.get('/', menuController.test);
+router.post('/', menuController.create);
 
 export default router;
