c04070e6747676c3a608def6f700e42ecb175b5a
Tunde Yakub <tundenasri@gmail.com>
2019-03-03 18:21:59 +0100

    secure meal routes

--- server/routes/meal.js
+++ server/routes/meal.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import passport from 'passport';
 import mealController from '../controllers/meal';
+import passportService from '../services/passport';
 
 const router = express.Router();
 
-router.post('/', mealController.create);
-router.get('/caterer/:id', mealController.list);
-router.put('/:id', mealController.editMeal);
-router.delete('/:id', mealController.delete);
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+router.post('/', requireAuth, mealController.create);
+router.get('/caterer/:id', requireAuth, mealController.list);
+router.put('/:id', requireAuth, mealController.editMeal);
+router.delete('/:id', requireAuth, mealController.delete);
 
 export default router;
