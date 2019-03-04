3dffd6854b7b6d2ef47b880e55da180f8a3dba79
Tunde Yakub <tundenasri@gmail.com>
2019-03-04 08:59:18 +0100

    write test for adding menu to the database

--- server/test/menu.js
+++ server/test/menu.js
@@ -27,6 +27,63 @@ describe('Test login', () => {
   });
 });
 
+
+describe('POST api/v1/menu', () => {
+  it('should return error on wrong api call', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/wrongapi')
+      .send()
+      .end((err, res) => {
+        expect(res.status).to.eql(404);
+        done();
+      });
+  });
+  it('should return error when no data is sent with the request', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/menu')
+      .send({})
+      .set('Authorization', token)
+      .end((err, res) => {
+        expect(res.body.status).to.eql(400);
+        expect(res.body).to.have.property('error');
+        done();
+      });
+  });
+  it('should return error when all the required data isnt sent along the request', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/menu')
+      .send({ name: 'Breakfast Classic' })
+      .set('Authorization', token)
+      .end((err, res) => {
+        expect(res.body.status).to.eql(400);
+        expect(res.body).to.have.property('error');
+        done();
+      });
+  });
+  it('should post the data succesfully when the details are sent correctly', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/menu')
+      .send({
+        name: 'Breakfast Classic',
+        price: '1200',
+        meals: ['rice', 'beans'],
+      })
+      .set('Authorization', token)
+      .end((err, res) => {
+        expect(res.body.status).to.eql(201);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.data).to.have.property('id');
+        expect(res.body.data).to.have.property('name');
+        expect(res.body.data).to.have.property('meals');
+        done();
+      });
+  });
+});
+
 describe('Menu tests', () => {
   describe('GET api/v1/menu', () => {
     it('should return error 404 on wrong api call', (done) => {
@@ -51,91 +108,39 @@ describe('Menu tests', () => {
     });
   });
 });
-//   describe('GET /api/v1/menu/id', () => {
-//     it('should return error 404 on wrong api call', (done) => {
-//       chai
-//         .request(server)
-//         .get('/api/v1/wrongapi')
-//         .end((err, res) => {
-//           expect(res.status).to.eql(404);
-//           done();
-//         });
-//     });
-//     it('should return error when wrong menu is passed', (done) => {
-//       chai
-//         .request(server)
-//         .get('/api/v1/menu/wrongid')
-//         .end((err, res) => {
-//           expect(res.body.status).to.eql(400);
-//           expect(res.body.data).to.have.property('message');
-//           done();
-//         });
-//     });
-//     it('should return the menu when correct id is passed', (done) => {
-//       chai
-//         .request(server)
-//         .get('/api/v1/menu/1')
-//         .end((err, res) => {
-//           expect(res.body.status).to.eql(200);
-//           expect(res.body.data).to.to.an('object');
-//           expect(res.body.data).to.have.property('id');
-//           expect(res.body.data).to.have.property('name');
-//           expect(res.body.data).to.have.property('meals');
-//           expect(res.body.data.meals).to.be.an('array');
-//           done();
-//         });
-//     });
+
+// describe('GET /api/v1/menu/id', () => {
+//   it('should return error 404 on wrong api call', (done) => {
+//     chai
+//       .request(server)
+//       .get('/api/v1/wrongapi')
+//       .end((err, res) => {
+//         expect(res.status).to.eql(404);
+//         done();
+//       });
 //   });
-//   describe('POST api/v1/menu', () => {
-//     it('should return error on wrong api call', (done) => {
-//       chai
-//         .request(server)
-//         .post('/api/v1/wrongapi')
-//         .send()
-//         .end((err, res) => {
-//           expect(res.status).to.eql(404);
-//           done();
-//         });
-//     });
-//     it('should return error when no data is sent with the request', (done) => {
-//       chai
-//         .request(server)
-//         .post('/api/v1/menu')
-//         .send({})
-//         .end((err, res) => {
-//           expect(res.body.status).to.eql(400);
-//           expect(res.body.data).to.have.property('message');
-//           done();
-//         });
-//     });
-//     it('should return error when all the required data isnt sent along the request', (done) => {
-//       chai
-//         .request(server)
-//         .post('/api/v1/menu')
-//         .send({ name: 'Breakfast Classic' })
-//         .end((err, res) => {
-//           expect(res.body.status).to.eql(400);
-//           expect(res.body.data).to.have.property('message');
-//           done();
-//         });
-//     });
-//     it('should post the data succesfully when the details are sent correctly', (done) => {
-//       chai
-//         .request(server)
-//         .post('/api/v1/menu')
-//         .send({
-//           name: 'Breakfast Classic',
-//           price: '1200',
-//           meals: ['rice', 'beans'],
-//         })
-//         .end((err, res) => {
-//           expect(res.body.status).to.eql(201);
-//           expect(res.body.data).to.be.an('object');
-//           expect(res.body.data).to.have.property('id');
-//           expect(res.body.data).to.have.property('name');
-//           expect(res.body.data).to.have.property('meals');
-//           done();
-//         });
-//     });
+//   it('should return error when wrong menu is passed', (done) => {
+//     chai
+//       .request(server)
+//       .get('/api/v1/menu/wrongid')
+//       .end((err, res) => {
+//         expect(res.body.status).to.eql(400);
+//         expect(res.body.data).to.have.property('message');
+//         done();
+//       });
+//   });
+//   it('should return the menu when correct id is passed', (done) => {
+//     chai
+//       .request(server)
+//       .get('/api/v1/menu/1')
+//       .end((err, res) => {
+//         expect(res.body.status).to.eql(200);
+//         expect(res.body.data).to.to.an('object');
+//         expect(res.body.data).to.have.property('id');
+//         expect(res.body.data).to.have.property('name');
+//         expect(res.body.data).to.have.property('meals');
+//         expect(res.body.data.meals).to.be.an('array');
+//         done();
+//       });
 //   });
 // });
