669fc3b2ca557c15a3a41bffe0091b68fd4a77a2
Tunde Yakub <tundenasri@gmail.com>
2019-03-04 08:35:46 +0100

    write test for login route

--- server/test/auth.js
+++ server/test/auth.js
@@ -6,20 +6,39 @@ import server from '../app';
 const { expect } = chai;
 chai.use(chaiHttp);
 
+const user = {
+  firstName: 'Bisi',
+  lastName: 'Bisiano',
+  email: 'bisi@mail.com',
+  password: 'lollipoppo',
+  roleId: 1,
+};
+
 describe('Test login endpoint', () => {
   it('register for an account', (done) => {
     chai
       .request(server)
       .post('/api/v1/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        expect(res.body).to.have.property('token');
+        done();
+      });
+  });
+});
+
+describe('Test login', () => {
+  it('should login in a user', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/auth/login')
       .send({
-        firstName: 'Bisi',
-        lastName: 'Bisiano',
-        email: 'bisi@mail.com',
-        password: 'lollipoppo',
-        roleId: 1,
+        email: user.email,
+        password: user.password,
       })
       .end((err, res) => {
         expect(res.body).to.have.property('token');
+        expect(res.body).to.have.property('name');
         done();
       });
   });
