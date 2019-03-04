642fe9a0c8c0f5dad2f1260e59d272a0caa50c49
Tunde Yakub <tundenasri@gmail.com>
2019-03-04 09:50:06 +0100

    write test for role route

--- server/test/auth.js
+++ server/test/auth.js
@@ -11,8 +11,35 @@ const user = {
   lastName: 'Bisiano',
   email: 'bisi@mail.com',
   password: 'lollipoppo',
-  roleId: 1,
+  role: 1,
 };
+
+describe('Set roles', () => {
+  it('should set admin role', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/role')
+      .send({ name: 'Admin' })
+      .end((err, res) => {
+        expect(res.body.status).to.eql(201);
+        done();
+      });
+  });
+});
+
+describe('Set roles', () => {
+  it('should set user role', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/role')
+      .send({ name: 'Customer' })
+      .end((err, res) => {
+        expect(res.body.status).to.eql(201);
+        done();
+      });
+  });
+});
+
 
 describe('Test login endpoint', () => {
   it('register for an account', (done) => {
