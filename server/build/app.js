e08b3f382938fc557213c99f861533f334df54e4
Tunde Yakub <tundenasri@gmail.com>
2019-03-05 22:07:14 +0100

    create new project build

--- server/build/app.js
+++ server/build/app.js
@@ -2,6 +2,8 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _path = _interopRequireDefault(require("path"));
+
 var _auth = _interopRequireDefault(require("./routes/auth"));
 
 var _meal = _interopRequireDefault(require("./routes/meal"));
@@ -14,10 +16,37 @@ var _role = _interopRequireDefault(require("./routes/role"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var swaggerJSDoc = require('swagger-jsdoc');
+
 require('dotenv').config(); // routes
 
 
-var app = (0, _express.default)();
+var app = (0, _express.default)(); // -- setup up swagger-jsdoc --
+
+var swaggerDefinition = {
+  info: {
+    title: 'Book a meal',
+    version: '1.0.0',
+    description: 'All things meal'
+  },
+  host: 'https://protected-waters-12782.herokuapp.com',
+  basePath: '/'
+}; // options for the swagger docs
+
+var options = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./server/routes/*.js']
+};
+var swaggerSpec = swaggerJSDoc(options);
+app.get('/docs', function (req, res) {
+  res.sendFile(_path.default.join(__dirname, 'redoc.html'));
+});
+app.get('/swagger.json', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 app.use(_express.default.json());
 app.use(_express.default.urlencoded({
   extended: false
--- server/build/routes/menu.js
+++ server/build/routes/menu.js
@@ -7,19 +7,70 @@ exports.default = void 0;
 
 var _express = _interopRequireDefault(require("express"));
 
+var _passport = _interopRequireDefault(require("passport"));
+
 var _menu = _interopRequireDefault(require("../controllers/menu"));
 
-var _passport = _interopRequireDefault(require("../services/passport"));
-
-var _passport2 = _interopRequireDefault(require("passport"));
+var _passport2 = _interopRequireDefault(require("../services/passport"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var router = (0, _express.default)();
 
-var requireAuth = _passport2.default.authenticate('jwt', {
+var requireAuth = _passport.default.authenticate('jwt', {
   session: false
 });
+/**
+ * @swagger
+ * definitions:
+ *   Menu:
+ *     properties:
+ *       name:
+ *         type: string
+ *       price:
+ *         type: integer
+ *       meals:
+ *         type: array
+ *       catererId:
+ *         type: integer
+ */
+
+/**
+ * @swagger
+ * /api/v1/menu:
+ *   get:
+ *     summary: List all the menu
+ *     description: Returns a list of all menu available
+ *     tags:
+ *       - Menu
+ *     responses:
+ *       200:
+ *         description: An array of menus
+ *         schema:
+ *           $ref: '#/definitions/Menu'
+ */
+
+/**
+ * @swagger
+ * /api/v1/menu:
+ *   post:
+ *     tags:
+ *       - Menu
+ *     description: Creates a new menu
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: menu
+ *         description: Menu object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Menu'
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ */
+
 
 router.get('/', requireAuth, _menu.default.list);
 router.post('/', requireAuth, _menu.default.create);
