427ba33bbf786304d24ec344f8dca02e1bc31861
Tunde Yakub <tundenasri@gmail.com>
2019-03-05 15:23:51 +0100

    create api documentation for the menu route

--- server/app.js
+++ server/app.js
@@ -1,15 +1,46 @@
 import express from 'express';
+import path from 'path';
 import authRoute from './routes/auth';
 import mealRoutes from './routes/meal';
 import orderRoutes from './routes/order';
 import menuRoutes from './routes/menu';
 import roleRoute from './routes/role';
 
+const swaggerJSDoc = require('swagger-jsdoc');
+
+
 require('dotenv').config();
 // routes
-
-
 const app = express();
+
+
+// -- setup up swagger-jsdoc --
+const swaggerDefinition = {
+  info: {
+    title: 'Book a meal',
+    version: '1.0.0',
+    description: 'All things meal',
+  },
+  host: 'localhost:3000',
+  basePath: '/',
+};
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ['./server/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.get('/docs', (req, res) => {
+  res.sendFile(path.join(__dirname, 'redoc.html'));
+});
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
--- server/routes/menu.js
+++ server/routes/menu.js
@@ -1,12 +1,63 @@
 import express from 'express';
+import passport from 'passport';
 import menuController from '../controllers/menu';
 import passportService from '../services/passport';
-import passport from 'passport';
 
 const router = express();
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 
+
+/**
+ * @swagger
+ * definitions:
+ *   Menu:
+ *     properties:
+ *       name:
+ *         type: string
+ *       price:
+ *         type: integer
+ *       meals:
+ *         type: array
+ *       catererId:
+ *         type: integer
+ */
+
+/**
+ * @swagger
+ * /api/v1/menu:
+ *   get:
+ *     summary: List all the menu
+ *     description: Returns a list of all menu available
+ *     tags:
+ *       - Menu
+ *     responses:
+ *       200:
+ *         description: An array of menus
+ *         schema:
+ *           $ref: '#/definitions/Menu'
+ */
+
+/**
+ * @swagger
+ * /api/v1/menu:
+ *   post:
+ *     tags:
+ *       - Menu
+ *     description: Creates a new menu
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: menu
+ *         description: Menu object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Menu'
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ */
 router.get('/', requireAuth, menuController.list);
 router.post('/', requireAuth, menuController.create);
 export default router;
